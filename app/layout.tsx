1afd35076fe70bc619527fc3e0c129bb1989cf67
Sameera Rajapakshe <svirajrajapaksha18@gmail.com>
2026-09-18 17:22:37 +0530

    update g tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import { GoogleAnalytics } from '@next/third-parties/google' // අලුත් import එක
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css"
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     "Professional web development, software solutions, and digital marketing services for travel agents and hotels.",
   generator: "v0.dev",
   icons: {
-    icon: "/images/logo.jpg", // This will use the favicon in the public folder
+    icon: "/images/logo.jpg", 
   },
 }
 
@@ -33,8 +33,8 @@ export default function RootLayout({
         <Footer />
         <WhatsAppButton />
 
-        {/* අලුත් Google Analytics Component එක */}
-        <GoogleAnalytics gaId="G-DYKRNND4XJ" />
+        {/* Update කරපු Google Analytics ID එක */}
+        <GoogleAnalytics gaId="G-S8Y9B5N5LG" />
         
       </body>
     </html>
